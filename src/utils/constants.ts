95089736d57932c873a0f16be27acf5e5b6d61e1
Jayakanth <jai06032005@gmail.com>
2025-10-12 00:13:19 +0530

    new

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,6 @@ export const COMPANY_LOGOS = {
   Google: '/assets/companies/google.png',
   Microsoft: '/assets/companies/microsoft.png',
   Amazon:'/assets/companies/amazon.jpg',
-  Apple: '/assets/companies/apple.png',
   Meta: '/assets/companies/meta.png',
   Tesla: '/assets/companies/tesla.png',
   CyberMinds: '/assets/cyberminds.png',
