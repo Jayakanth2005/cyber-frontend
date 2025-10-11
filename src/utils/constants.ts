9f4336ec439badd36c3113c356800afbe0bebea5
Jayakanth <jai06032005@gmail.com>
2025-10-11 22:54:25 +0530

    adding assets

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const COMPANY_LOGOS = {
   'Google': '/assets/companies/google.png',
   'Microsoft': '/assets/companies/microsoft.png',
-  'Amazon': '/assets/companies/amazon.png',
+  'Amazon': '/assets/companies/amazon.jpg',
   'Apple': '/assets/companies/apple.png',
   'Meta': '/assets/companies/meta.png',
   'Tesla': '/assets/companies/tesla.png',
